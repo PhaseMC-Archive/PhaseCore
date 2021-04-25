9203fd48d7b13dc7b2633efa6d1976f668dedfbb
Jordan Adams <jordan@jordanplayz158.me>
2021-04-25 09:41:59 -0400

    Test.java - Optimized Imports (Removed Unused Imports)

--- src/main/java/net/phasemc/core/scripts/Test.java
+++ src/main/java/net/phasemc/core/scripts/Test.java
@@ -3,7 +3,6 @@ package net.phasemc.core.scripts;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
-import org.bukkit.Color;
 
 public class Test {
     public Test(Player player, Command cmd, String label, String[] args, String arg) {
