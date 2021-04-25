782dbd79a9ed4a2097302dacc18f575476bb6bf8
Jordan Adams <jordan@jordanplayz158.me>
2021-04-25 09:41:36 -0400

    Main.java - Fixed Annotations - Added Main#playerCheck method due to CommandHandler removal.

--- src/main/java/net/phasemc/core/Main.java
+++ src/main/java/net/phasemc/core/Main.java
@@ -1,55 +1,42 @@
 package net.phasemc.core;
 
-import net.phasemc.core.commands.CoreCommand;
-import net.phasemc.core.essentials.Gamemodes;
-import org.bukkit.plugin.java.JavaPlugin;
 import net.phasemc.core.commands.Script;
+import net.phasemc.core.essentials.Flight;
+import net.phasemc.core.essentials.Gamemodes;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.annotation.command.Command;
+import org.bukkit.plugin.java.annotation.command.Commands;
 import org.bukkit.plugin.java.annotation.plugin.Description;
 import org.bukkit.plugin.java.annotation.plugin.LogPrefix;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import org.bukkit.plugin.java.annotation.plugin.Website;
 import org.bukkit.plugin.java.annotation.plugin.author.Author;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import org.bukkit.plugin.java.annotation.plugin.author.Authors;
 
 
 //Main plugin declaration
 @Plugin(name = "THE-CORE", version = "1.0")
 @Description(desc = "The main core for the phase-mc server.")
-@Author(name = "shmezi")
-@Author(name = "Jordan")
-@Author(name = "Other staff")
+@Authors({@Author(name = "shmezi"), @Author(name = "JordanPlayz158")})
 @LogPrefix(prefix = "CORE")
 @Website(url = "http://phasemc.net")
 //Commands start here
-@Command(name = "gmsp")
-@Command(name = "gms")
-@Command(name = "gmc")
-@Command(name = "gma")
+@Commands({@Command(name = "gmsp"), @Command(name = "gms"), @Command(name = "gmc"), @Command(name = "gma")})
 public class Main extends JavaPlugin {
-
     private static Main main;
-    private List<CoreCommand> commands;
 
     @Override
     public void onEnable(){
         main = this;
 
-        CommandHandler commandHandler = new CommandHandler();
-
-        commands = new ArrayList<>();
-        commands.addAll(Collections.singletonList(new Gamemodes()));
-
-        this.getCommand("script").setExecutor(commandHandler);
-        this.getCommand("gmc").setExecutor(commandHandler);
-        this.getCommand("gms").setExecutor(commandHandler);
-        this.getCommand("gmsp").setExecutor(commandHandler);
-        this.getCommand("gma").setExecutor(commandHandler);
-        this.getCommand("fly").setExecutor(commandHandler);
+        this.getCommand("script").setExecutor(new Script());
+        this.getCommand("gmc").setExecutor(new Gamemodes());
+        this.getCommand("gms").setExecutor(new Gamemodes());
+        this.getCommand("gmsp").setExecutor(new Gamemodes());
+        this.getCommand("gma").setExecutor(new Gamemodes());
+        this.getCommand("fly").setExecutor(new Flight());
 
         System.out.println("THE-CORE HAS BEEN ENABLED!");
     }
@@ -58,7 +45,11 @@ public class Main extends JavaPlugin {
         return main;
     }
 
-    public List<CoreCommand> getCommands() {
-        return commands;
+    public static Player playerCheck(CommandSender sender) {
+        if(sender instanceof Player) {
+            return (Player) sender;
+        } else {
+            return null;
+        }
     }
 }
